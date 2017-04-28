0de6a36ca4775f53c9c73ca5c3379f9617fd2268
Alejandro Pozo Huertas <apozohue10@gmail.com>
2017-04-28 13:29:38 +0200

    First commit TFM

---
+++ Chocolate_Factory_2.0/images/IdM/docker-entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Author: Alejandro Pozo Huertas
+# Entrypoint for IDM in chocolate factory application
+
+# Get ip of the chocolate factory
+function ip_app () {
+
+  echo "---Ips de app---"
+  VARIPAPP=`ping -c 1 chocolatefactory | grep -m 1 "" | cut -d "(" -f2 | cut -d ")" -f1`
+  echo "$VARIPAPP"
+  export VARIPAPP
+
+}
+
+# Provide consumer, users and roles to the application
+function _data_provision () {
+        echo "---Database---"
+        VARTAM=`du /keystone/keystone.db`
+        arrayTam=$(echo $VARTAM | tr "  " "\n")
+
+        VARPAS="true"
+        for x in $arrayTam
+        do
+           if [ $VARPAS == "true" ]; then
+              VARNS=$x
+              VARPAS="false"
+           fi
+        done
+
+        if [ -e /keystone/keystone.db ] && [  $VARNS -gt 112 ]; then
+           echo "---Provision has been done already---"
+        else
+           python default_provision.py
+        fi
+}
+
+# Run Keystone and Horizon
+ip_app
+
+echo "---Run Keystone---"
+./tools/with_venv.sh bin/keystone-all ${KEYSTONE_VERBOSE_LOG} >> /var/log/keystone.log 2>&1 &
+
+sleep 5
+
+_data_provision
+
+echo "---Run Horizon---"
+sudo /horizon/tools/with_venv.sh python /horizon/manage.py runserver 0.0.0.0:8000
+
