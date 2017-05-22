dfbf09ac764f5e686d26eb5e13414b7f665e8c2d
Alejandro Pozo Huertas <apozohue10@gmail.com>
2017-05-22 13:52:17 +0200

    3er version

--- Chocolate_Factory_3.0/Docker-Images/App/chocolatefactory/config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.idmURL = 'http://localhost:8000';
-config.client_id = '5d43e494421946edbb2add4a630f34f2';
-config.client_secret = '76eafec1c7a545faa07a9007ad523fb9';
-config.callbackURL = 'http://localhost:1028/login';
-
-module.exports = config;
